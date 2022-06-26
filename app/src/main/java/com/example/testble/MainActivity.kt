a7ca3edba261e9e557ccea221ec483b47f52f492
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2022-06-27 02:10:26 +0900

    リアルタイムスキャンOK

--- app/src/main/java/com/example/testble/MainActivity.kt
+++ app/src/main/java/com/example/testble/MainActivity.kt
@@ -1,52 +1,55 @@
 package com.example.testble
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.*
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat.checkSelfPermission
-import androidx.core.content.PermissionChecker.checkSelfPermission
 
 class MainActivity : AppCompatActivity() {
-
-    @SuppressLint("MissingPermission")
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private val requestEnableBT = 1
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val manager: BluetoothManager =
-            getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        val adapter = manager.adapter
-        val bluetoothLeScanner: BluetoothLeScanner = adapter.bluetoothLeScanner
-
-
-        var scanFilter: ScanFilter = ScanFilter.Builder()
-            .build()
-        var scanFilterList: ArrayList<ScanFilter> = ArrayList()
+        val manager: BluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = manager.adapter
+        // 端末がBluetoothサポートされてる？
+        if (bluetoothAdapter == null) {
+            Toast.makeText(applicationContext, "端末サポートされてません", Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+        // bluetoothは　有効になってる？
+        // 無効なら有効にしていいか許可をとるダイアログを表示する。
+        if (!bluetoothAdapter.isEnabled) {
+            Toast.makeText(applicationContext, "Bluetoothが無効になっています", Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+        val bluetoothLeScanner: BluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        val scanFilter: ScanFilter = ScanFilter.Builder().build()
+        val scanFilterList: ArrayList<ScanFilter> = ArrayList()
         scanFilterList.add(scanFilter)
-
-        var scanSettings: ScanSettings = ScanSettings.Builder()
-            .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
-            .build()
+        val scanSettings: ScanSettings = ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_BALANCED).build()
         Log.d("TAG", "startScan set")
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN), requestEnableBT)
+        }
         bluetoothLeScanner.startScan(scanFilterList, scanSettings, scanCallback)
     }
-
     //スキャンで見つかったデバイスが飛んでくる
-    private val scanCallback: ScanCallback = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    object : ScanCallback() {
+    private val scanCallback: ScanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
-            Log.d("scanResult:", ",address,${result.device.address},rssi,${result.rssi}")
+            Log.d("scanResult:", result.device.address)
         }
     }
 }
