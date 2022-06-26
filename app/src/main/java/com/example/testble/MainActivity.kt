a953c319c4cde2be278b6a5388f35b785687f03f
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2022-06-27 05:08:47 +0900

    一旦コミット

--- app/src/main/java/com/example/testble/MainActivity.kt
+++ app/src/main/java/com/example/testble/MainActivity.kt
@@ -5,35 +5,39 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.le.*
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.Nullable
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.cardiomood.android.controls.gauge.SpeedometerGauge
+import java.security.AccessController.getContext
 
 class MainActivity : AppCompatActivity() {
     private val requestEnableBT = 1
+    //private val speedMeter:SpeedometerGauge = findViewById(R.id.speedometer)
+    private val speedMeter: SpeedometerGauge? = null
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val manager: BluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         val bluetoothAdapter = manager.adapter
-        // 端末がBluetoothサポートされてる？
         if (bluetoothAdapter == null) {
             Toast.makeText(applicationContext, "端末サポートされてません", Toast.LENGTH_LONG).show()
             finish()
             return
         }
-        // bluetoothは　有効になってる？
-        // 無効なら有効にしていいか許可をとるダイアログを表示する。
         if (!bluetoothAdapter.isEnabled) {
             Toast.makeText(applicationContext, "Bluetoothが無効になっています", Toast.LENGTH_LONG).show()
             finish()
             return
         }
+        setSpeedmeterGauge()
         val bluetoothLeScanner: BluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
         val scanFilter: ScanFilter = ScanFilter.Builder().build()
         val scanFilterList: ArrayList<ScanFilter> = ArrayList()
@@ -43,13 +47,26 @@ class MainActivity : AppCompatActivity() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN), requestEnableBT)
         }
+        Log.d("mainactivity:", getContext().toString())
         bluetoothLeScanner.startScan(scanFilterList, scanSettings, scanCallback)
     }
     //スキャンで見つかったデバイスが飛んでくる
     private val scanCallback: ScanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
+            Log.d("scanResult:", getContext().toString())
             Log.d("scanResult:", result.device.address)
         }
+    }
+    private fun setSpeedmeterGauge() {
+        if (speedMeter == null) {
+            return
+        }
+        speedMeter.maxSpeed = 300.0
+        speedMeter.majorTickStep = 30.0
+        speedMeter.minorTicks = 2
+        speedMeter.addColoredRange(30.0,140.0, Color.GREEN)
+        speedMeter.addColoredRange(140.0,180.0, Color.YELLOW)
+        speedMeter.addColoredRange(180.0,400.0, Color.RED)
     }
 }
